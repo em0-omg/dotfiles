a443e3b9b9fa0e69a17b850874d5e82ab71ebc67
em0-omg <em0818.omg@gmail.com>
2022-01-06 16:23:40 +0900

    add ohmyzsh config, and neofetch

--- update-script.sh
+++ update-script.sh
@@ -2,6 +2,7 @@
 cp ./init.vim ~/.config/nvim/init.vim
 cp ./.tmux.conf ~/.tmux.conf
 cp ./.zshrc ~/.zshrc
+cp ./config.conf ~/.config/neofetch/config.conf
 
 # 秘密の内容は追記
 cat ./.zshrc_secrets >> ~/.zshrc
